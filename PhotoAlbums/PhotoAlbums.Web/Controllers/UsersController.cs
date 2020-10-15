f5f32cbbc5f0a1a28309ada4c65007a9cabcdf92
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-15 15:59:01 -0400

    User show page is now functional, does not yet show posts.

---
+++ PhotoAlbums/PhotoAlbums.Web/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using PhotoAlbums.Web.Services;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PhotoAlbums.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        [Route("users/{id}")]
+        public async Task<ActionResult> Show(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var user = await ApiService.GetUserAsync((int)id);
+
+            return View(user);
+        }
+    }
+}
