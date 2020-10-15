bcfa66935cd4d096c3ddfcce88d1908a6c0cbace
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-15 11:28:04 -0400

    Added a couple of data transfer objects.

---
+++ PhotoAlbums/PhotoAlbums.Web/Models/Dtos/AlbumDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoAlbums.Web.Models.Dtos
+{
+    public class AlbumDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
