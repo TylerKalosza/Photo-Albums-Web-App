1369bb5829d00b51e6224a5db74504cede44c9ae
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-15 16:16:30 -0400

    Added Post model and Post data transfer object.

---
+++ PhotoAlbums/PhotoAlbums.Web/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoAlbums.Web.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
