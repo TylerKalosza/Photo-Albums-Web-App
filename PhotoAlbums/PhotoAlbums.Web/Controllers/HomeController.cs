dc7bb43fc0f45eaa07404c017b20fa73ab6dc116
Tyler Kalosza <tyler.kalosza@gmail.com>
2020-10-15 12:46:55 -0400

    Added search functionality to homepage.

--- PhotoAlbums/PhotoAlbums.Web/Controllers/HomeController.cs
+++ PhotoAlbums/PhotoAlbums.Web/Controllers/HomeController.cs
@@ -8,12 +8,20 @@ namespace PhotoAlbums.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index()
+        public async Task<ActionResult> Index(string searchString)
         {
             var albums = await ApiService.GetAlbumsAsync();
 
+            // Filter the data based on the search string matching part of the album title or user's name.
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                albums = albums.Where(album => album.Title.Contains(searchString.ToLower())
+                    || (album.User != null && album.User.Name.ToLower().Contains(searchString.ToLower())));
+            }
+
             var photos = await ApiService.GetPhotosAsync(albums);
 
+            // Create the AlbumViewModels that will be used in the view.
             var albumVms = albums.Select(album => new AlbumViewModel(
                 album,
                 photos.FirstOrDefault(photo => photo.Album?.Id == album.Id)?.ThumbnailUrl)
